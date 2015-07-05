048d857c46737fbc36d63c8bf3b4f828676c21f5
Nick Ostrovsky <nick@firedev.com>
2015-07-05 13:56:43 +0700

    knife.rb

---
+++ .chef/knife.rb
@@ -0,0 +1,23 @@
+my_orgname = ENV['OPSCODE_ORGNAME']
+my_chef_server_url = ENV['KNIFE_CHEF_SERVER']
+my_cookbook_copyright = ENV['KNIFE_COOKBOOK_COPYRIGHT']
+my_cookbook_license = ENV['KNIFE_COOKBOOK_LICENSE']
+my_cookbook_email = ENV['KNIFE_CACHE_PATH']
+current_dir = File.dirname(__FILE__)
+
+node_name ENV['OPSCODE_USER']
+client_key "#{ENV['KNIFE_VALIDATION_KEY_FOLDER']}/#{ENV['OPSCODE_USER']}.pem"
+validation_client_name "#{my_orgname}-validator"
+validation_key "#{ENV['KNIFE_VALIDATION_KEY_FOLDER']}/#{my_orgname}-validator.pem"
+chef_server_url my_chef_server_url
+
+cache_type 'BasicFile'
+cache_options(path: ENV['KNIFE_CACHE_PATH'])
+
+log_level :info
+log_location STDOUT
+
+cookbook_path ["#{current_dir}/../cookbooks"]
+cookbook_copyright my_cookbook_copyright
+cookbook_license my_cookbook_license
+cookbook_email my_cookbook_email
